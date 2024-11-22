6133bbab293dd7aaa522e4244fae60b3ccb1e6ac
Pavlo-Chernicenko <pwned7067@gmail.com>
2024-11-23 00:10:46 +0200

    test

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './js/projects';
-import './js/faq';
-import './js/reviews';
-import './js/work-together';
-
-import './js/mob-menu';
-import './js/modal-window';
-
-import './js/changes-mod';
